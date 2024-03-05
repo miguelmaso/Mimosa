9eeb23dff50c19442e5486e919592f0e754eae1d
Alberto-PE <73haldir73@gmail.com>
2024-03-06 00:17:08 +0100

    Init

---
+++ Flux_Calibration/Line_Fitting.jl
@@ -0,0 +1,66 @@
+using Flux
+using Flux: train!
+using Statistics
+using Plots
+#-------------------------------------------------------------------------------
+# Test and training data
+#-------------------------------------------------------------------------------
+actual(x) = 4x +2
+x_train, x_test = hcat(0:5...), hcat(6:10...)
+y_train, y_test = actual.(x_train), actual.(x_test)
+
+#-------------------------------------------------------------------------------
+# Build a model. Now it's just a simple layer with one input and one output
+#-------------------------------------------------------------------------------
+model = Dense(1 => 1) # Specified sigmoid activation function
+model.weight # Initialized weight
+model.bias   # Initialized bias
+
+#-------------------------------------------------------------------------------
+# Train the model
+#-------------------------------------------------------------------------------
+#loss(model, x, y) = mean(abs2.(model(x) .- y)); #Loss function. This is the MSE, but could be a Flux.Crossentropy or any other function
+function loss(flux_model, x, y) # Alternative definition using Flux's version of the MSE
+    ŷ = flux_model(x)
+    Flux.mse(ŷ, y)
+end;
+
+initial_loss = loss(model, x_train, y_train)
+printstyled("The initial loss is $initial_loss \n"; color = :red)
+opt = Descent() # Define an optimisation strategy. In this case, just the gradient descent. But could de Adams, etc. 
+printstyled("The learning rate of the gradient descent is $opt \n"; color = :green)
+
+data = [(x_train,y_train)]
+# Now we iteratively train the model with the training data, minimizing the loss function by updating the weights and biases following the gradient descent
+function iterative_training(model, x_train, y_train, tol)
+epoch = 1
+iter = 1
+Weights = zeros(0)
+Biases = zeros(0)
+    while loss(model, x_train, y_train) > tol && iter<500
+     train!(loss, model, data, opt)
+     L = loss(model, x_train, y_train)
+     println("Epoch number $epoch with a loss $L ")
+     iter += 1
+     epoch += 1
+     #-------------------
+     #Store the gradients
+     #-------------------
+     dLdm, _, _ = gradient(loss,model, x_test, y_test)
+     append!(Weights, dLdm.weight)
+     append!(Biases, dLdm.bias)
+    end
+return Weights, Biases
+end
+Weights , Biases= iterative_training(model, x_train, y_train, 1e-6)
+
+
+#-------------------------------------------------------------------------------
+# Predict the model
+#-------------------------------------------------------------------------------
+
+printstyled("Predicted data is: \n"; color= :green)
+println(model(x_test))
+printstyled("Actual data is:\n"; color= :green)
+println(y_test)
+plot(vec(x_test),[vec(y_test),vec(model(x_test))], seriestype = :scatter, label=["Original Test" "Fitting Results"])
