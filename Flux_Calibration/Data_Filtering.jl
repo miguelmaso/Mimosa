7bd88b4768bb025b6f8e8bdda0f3c5bb935ba685
Alberto-PE <73haldir73@gmail.com>
2024-03-18 20:41:19 +0100

    R2 update

--- Flux_Calibration/Data_Filtering.jl
+++ Flux_Calibration/Data_Filtering.jl
@@ -10,5 +10,24 @@ using DelimitedFiles
 x_train = readdlm("Input_Alberto_.txt", ',');
 y_train = readdlm("Output_Alberto_.txt", ',');
 
+x_train = x_train'
+y_train = y_train'
 
-plot(y_train, seriestype = :scatter, label=["Original Data"])
+function sort_with_indices(arr)
+    # Create a copy of the original array
+    sorted_arr = copy(arr)
+    
+    # Sort the array in ascending order
+    sort!(sorted_arr, dims=2)
+    
+    # Find the indices that were changed
+    indices_changed = findall(x -> arr[x] != sorted_arr[x], 1:length(arr))
+    
+    return sorted_arr, indices_changed
+end
+
+
+sorted_y_train, indices = sort_with_indices(y_train)
+
+
+#plot(y_train, seriestype = :scatter, label=["Original Data"])
--- Flux_Calibration/NN_Rogelio_Data.jl
+++ Flux_Calibration/NN_Rogelio_Data.jl
@@ -7,59 +7,110 @@ using DelimitedFiles
 #-------------------------------------------------------------------------------
 # Test and training data
 #-------------------------------------------------------------------------------
-x_train = readdlm("Input_Alberto_.txt", ',')
-x_test = readdlm("Input_Alberto_.txt", ',')
-y_train = readdlm("Output_Alberto_.txt", ',')
-y_test = readdlm("Output_Alberto_.txt", ',')
+x_train = readdlm("Input_Alberto_3.txt", ',')
+x_test = readdlm("Input_Alberto_3.txt", ',')
+y_train = readdlm("Output_Alberto_3.txt", ',')
+y_test = readdlm("Output_Alberto_3.txt", ',')
 
-x_train = x_train[1:30,:]'
-y_train = y_train[1:30]'
-x_test = x_test[1:30,:]'
-y_test = y_test[1:30]'
+#x_train_n = hcat(x_train[:,1:2],x_train[:,4:5])
+x_train_n = x_train[:,1:2]
+y_train_n= y_train[:,1]
+@show size(x_train_n)
+@show size(y_train_n)
+
+x_train = x_train_n'
+y_train = y_train_n'
+x_test = x_test'
+y_test = y_test'
+@show size(x_train)
+@show size(y_train)
+
+@inline function normalise(x::AbstractArray; dims=ndims(x), ϵ=1e-8)
+  μ = mean(x, dims=dims)
+  σ = std(x, dims=dims, mean=μ, corrected=false)
+ #  return @. (x - μ) / (σ + ϵ)
+ return x
+end
+
+x_train_norm=normalise(x_train)
+y_train_norm=normalise(y_train)
+
+
+
 
 #-------------------------------------------------------------------------------
 # Build a model. Now it's just a simple layer with one input and one output
 #-------------------------------------------------------------------------------
 #Let's create a multi-layer perceptron
 model = Chain(
-    Dense(5=>3),
-    BatchNorm(3),
-    Dense(3=>3),
-    BatchNorm(3),
-    Dense(3 => 1)
+   Dense(2=>6, softplus),
+   BatchNorm(6),
+   Dense(6=>6,softplus),
+   BatchNorm(6),
+   Dense(6=>6,softplus),
+   BatchNorm(6),
+   Dense(6=>6,softplus),
+   BatchNorm(6),
+   Dense(6=>6,softplus),
+   BatchNorm(6),
+   Dense(6=>1,softplus),
 )
 
 
 
-#model = Dense(5 => 1) # Specified sigmoid activation function
+#model = Dense(5 => 1)  
 
 #-------------------------------------------------------------------------------
 # Train the model
 #-------------------------------------------------------------------------------
-#loss(model, x, y) = mean(abs2.(model(x) .- y)); #Loss function. This is the MSE, but could be a Flux.Crossentropy or any other function
-function loss(flux_model, x, y) # Alternative definition using Flux's version of the MSE
+# function loss(flux_model, x, y) # Alternative definition using Flux's version of the MSE
+#     ŷ = flux_model(x)
+#     Flux.mse(ŷ, y)
+# end;
+function loss(flux_model,x,y)
     ŷ = flux_model(x)
-    Flux.mse(ŷ, y)
+    num = sum((ŷ - y).^2)
+    den = sum((y.^2))
+    out = num/den
 end;
-function evaluation(flux_model, x ) # Alternative definition using Flux's version of the MSE
-    y_hat = flux_model(x)
-    return y_hat
-end;
-initial_loss = loss(model, x_train, y_train)
+function r2d2(actual_values, predicted_values) 
+    # Compute the mean of actual values
+    mean_actual = mean(actual_values)
+
+    # Compute the sum of squares of residuals
+    SS_res = sum((actual_values .- predicted_values).^2)
+
+    # Compute the total sum of squares
+    SS_tot = sum((actual_values .- mean_actual).^2)
+
+    # Compute R-squared (R2) error
+    R2 = 1 - SS_res / SS_tot
+
+    return R2
+end
+
+
+
+
+
+initial_loss = loss(model, x_train_norm, y_train_norm)
 printstyled("The initial loss is $initial_loss \n"; color = :red)
-opt = Descent(0.1) # Define an optimisation strategy. In this case, just the gradient descent. But could de Adams, etc. 
+#opt = Descent(0.02) # Define an optimisation strategy. In this case, just the gradient descent. But could de Adams, etc. 
+opt = Flux.setup(Adam(0.01), model)
 printstyled("The learning rate of the gradient descent is $opt \n"; color = :green)
 
-data = [(x_train,y_train)]
+data = [(x_train_norm,y_train_norm)]
 # Now we iteratively train the model with the training data, minimizing the loss function by updating the weights and biases following the gradient descent
 function iterative_training(model, x_train, y_train)
     epoch = 1
     iter = 1
     Losses = zeros(0)
-    while  epoch<1000
+    while  loss(model,x_train,y_train)>0.00001
      train!(loss, model, data, opt)
      L = loss(model, x_train, y_train)
      println("Epoch number $epoch with a loss $L ")
+     R2=r2d2(model(x_train_norm),y_train_norm)
+     println("R2  is $R2")
      iter += 1
      epoch += 1
      #-------------------
@@ -69,24 +120,61 @@ function iterative_training(model, x_train, y_train)
 #     append!(Weights, dLdm.weight)
 #     append!(Biases, dLdm.bias)
 #     append!(GradientX, dLdx)
+      plot([vec(y_train_norm),vec(model(x_train_norm))], seriestype = :scatter, label=["Original Test" "Fitting Results"])
       append!(Losses,L)
     end
     return Losses
 end
 
-Losses=iterative_training(model, x_train, y_train)
+Losses=iterative_training(model, x_train_norm, y_train_norm)
 
 
 #-------------------------------------------------------------------------------
 # Predict the model
 #-------------------------------------------------------------------------------
 #_, dLdx, _= gradient(loss,model, x_test, y_test)
-new_model(x) = sum(model(x)) # This is to obtain a scalar output for the model; which is a vector by default
-grad = gradient(new_model, x_test)
+#new_model(x) = sum(model(x)) # This is to obtain a scalar output for the model; which is a vector by default
+#grad = gradient(new_model, x_test)
 #println(grad)
+
+
+#-------------------------------------------------------------------------------
+# Sort y_train and find its indices. Then use those indices in the predicted output
+#-------------------------------------------------------------------------------
+
+function sort_and_apply_indices(original_arr, apply_arr)
+    # Create a copy of the original array
+    sorted_arr = copy(original_arr)
+    
+    # Sort the array in ascending order
+    sort!(sorted_arr,dims=2)
+    
+    # Find the indices that were changed
+    indices_changed = sortperm(original_arr,dims=2)
+    
+    # Apply the indices to another array
+    result_arr = similar(apply_arr, length(apply_arr))
+    for (i, idx) in enumerate(indices_changed)
+        result_arr[i] = apply_arr[idx]
+    end
+    
+    return sorted_arr, indices_changed, result_arr
+end
+
+y_predicted = model(x_train_norm)
+sorted_y_train, indices_y_train, sorted_y_predicted = sort_and_apply_indices(y_train_norm, y_predicted)
+
+
+
+
+
 
 #printstyled("Predicted data is: \n"; color= :green)
 #println(model(x_test))
 #printstyled("Actual data is:\n"; color= :green)
 #println(y_test)
-plot([vec(y_train),vec(model(x_train))], seriestype = :scatter, label=["Original Test" "Fitting Results"])
+plot([vec(y_train_norm),vec(model(x_train_norm))], label=["Original Test" "Fitting Results"])
+
+# num = ∑(y_i - y_i*)^2
+# den = ∑(y_i*)^2
+# y* es la analitica
