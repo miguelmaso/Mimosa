f9d4b68e9a5d6a6ceae5d3c9e25192d71469e01d
mbarillasvelasquez <mbarillasvelasquez@alumni.scu.edu>
2024-05-27 11:47:42 +0200

    Update to run for kPCA data

--- scripts/ex_stat_EM_PB_4S_SIUnits_2.jl
+++ scripts/ex_stat_EM_PB_4S_SIUnits_2.jl
@@ -2,7 +2,8 @@ using DrWatson
 using Mimosa
 using Gridap
 using Plots
-
+using CSV
+using DataFrames
 
 function get_parameters(n_sec, sw, pot)
 
@@ -88,7 +89,7 @@ end
 
 function plt_cl(ph,sw)
   uh = ph[1]
-  t = 0.0:0.1e-3:100.0e-3
+  t = 0.0:1e-3:100.0e-3
   X = [VectorValue(i,4.0e-3,0.4e-3) for i in t]
   uh_l = uh.(X)
   x = X + uh_l
@@ -96,7 +97,8 @@ function plt_cl(ph,sw)
   X1 = [X[i][1] for i in 1:length(x)]
   z1 = [x[i][3] for i in 1:length(x)]
   x1 = [x[i][1] for i in 1:length(x)]
-  return plot([x1,X1], [z1,Z1], title=sw, xticks=[0.0,0.05,0.1])
+  pl_ = plot([x1,X1], [z1,Z1], title=sw, xticks=[0.0,0.05,0.1])
+  return pl_, z1
 end
 
 conf = []
@@ -105,14 +107,19 @@ for i in 0:2, j in 0:2, k in 0:2, l in 0:2
 end
 
 n_sec = 4
-pot = 2000.0
+pot = 4000.0
 #sw = [0, 1, 1, 0] #0 = bottomsurf_   ;  1 = topsurf_; else = no potential in that section
 uh_ = []
 pl = []
-for i in 26:54
+z = []
+for i in 28:54
   sw = conf[i]
   ph, chache = main(; get_parameters(n_sec, sw, pot)...)
+  pl_, z_ = plt_cl(ph,sw)
   push!(uh_,ph[1])
-  push!(pl,plt_cl(ph,sw))
+  push!(pl,pl_)
+  push!(z,z_)
 end
-plot(pl...,layout=(3,9))
+# plot(pl...,layout=(3,9)) # requires to setup plot parameter to get it right
+df = DataFrame(z, :auto)
+CSV.write("data/csv/EM_PB_4S_2.csv", df)
