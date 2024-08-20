b63b3801f2f0d472602e65560fbf92c8fe3f7472
mbarillasvelasquez <mbarillasvelasquez@alumni.scu.edu>
2024-08-20 11:43:37 +0200

    Update to App_compiler

--- scripts/MB Ex/App_Compiler.jl
+++ scripts/MB Ex/App_Compiler.jl
@@ -1,3 +1,4 @@
 using PackageCompiler
 
-create_app(".", "Mimosa_TestRunApp", precompile_execution_file="C:/Users/mjbarillas/Documents/GitHub/Mimosa/scripts/MB Ex/TB ST4 SL4/ex0_stat_EM_TubeBeam_St&Sl.jl")
+create_app(".", "Mimosa_TestRunApp_0",force=true)
+# , precompile_execution_file="C:/Users/mjbarillas/Documents/GitHub/Mimosa/scripts/MB Ex/TB ST4 SL4/ex0_stat_EM_TubeBeam_St&Sl.jl")
