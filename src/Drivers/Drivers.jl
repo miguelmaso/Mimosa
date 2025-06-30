b20e56a4cfeb9e382d4225da810ea217659bb25a
Miguel Masó <miguel.maso@upc.edu>
2025-06-30 11:58:50 +0200

    replace mkdir by mkpath

--- src/Drivers/Drivers.jl
+++ src/Drivers/Drivers.jl
@@ -65,10 +65,10 @@ end
 
 function setupfolder(folder_path::String)
   if !isdir(folder_path)
-    mkdir(folder_path)
+    mkpath(folder_path)
   else
     rm(folder_path,recursive=true)
-    mkdir(folder_path)
+    mkpath(folder_path)
   end
 end
 
